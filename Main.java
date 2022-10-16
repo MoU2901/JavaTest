9ce01153007a8d3622a02b14b743fec4b3649863
MoU2901 <mo290103@gmail.com>
2022-10-16 16:19:11 +0200

    Bubble sort

--- Main.java
+++ Main.java
@@ -1,5 +1,32 @@
 public class Main{
+
+    static void Print(int[] table)
+    {
+        for(int x: table)
+            System.out.println(x);
+    }
+
+    static int[] Sortuj(int[] table){
+        for(int j=0; j<table.length; j++)
+        {
+            for(int i=0; i<table.length-j-1; i++)
+            {
+                if(table[i]>table[i+1])
+                {
+                    int temp;
+                    temp=table[i];
+                    table[i]=table[i+1];
+                    table[i+1]=temp;
+                }
+            }
+        }
+        return table;
+    }
     public static void main(String[] args){
-        System.out.println("Hello git");
+        int[] table = {1,5,4,2,-4,23,245,12,-213,-12,-43,-543};
+        Print(table);
+        table = Sortuj(table);
+        System.out.println("Po sortowaniu:");
+        Print(table);
     }
 }
